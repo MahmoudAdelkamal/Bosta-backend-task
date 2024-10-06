65e3b98aa46a003f4e6e9a4cc24d705c9159ea56
MahmoudAdelKamal <mahmoudadel2310@gmail.com>
2024-10-06 23:12:14 +0300

    feat: add the book entity class

---
+++ library-management-system/src/book/entity/book.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('books')
+export class Book {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  author: string;
+
+  @Column({ unique: true })
+  isbn: string;
+
+  @Column()
+  availableQuantity: number;
+
+  @Column()
+  shelfLocation: string;
+}
